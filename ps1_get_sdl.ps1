f24e65e2fcc35cdd9e17a7b32c4c2880c5c3cd4e
umut <umut8ergin@gmail.com>
2025-04-04 08:05:29 +0300

    win build ok + sdl get scripts

--- ps1_get_sdl.ps1
+++ ps1_get_sdl.ps1
@@ -1,5 +1,6 @@
-#!/bin/bash
 mkdir vendored
 git clone https://github.com/libsdl-org/SDL --branch=release-3.2.8 ./vendored/SDL
 git clone https://github.com/libsdl-org/SDL_image --branch=release-3.2.4 ./vendored/SDL_image
 git clone https://github.com/libsdl-org/SDL_ttf --branch=release-3.2.0 ./vendored/SDL_ttf
+.\vendored\SDL_image\external\Get-GitModules.ps1
+.\vendored\SDL_ttf\external\Get-GitModules.ps1
