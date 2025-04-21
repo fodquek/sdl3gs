c210b12abed79d82a263dd5f1cfc49cc46c2a70a
umut <umut8ergin@gmail.com>
2025-04-21 11:32:50 +0300

    handler stuff

--- demo.cpp
+++ demo.cpp
@@ -23,21 +23,59 @@
 constexpr std::string_view FONT_FILE{"./assets/fonts/OpenSans-Regular.ttf"};
 HGS::Font* defont{nullptr};
 
-void fillDemoScene(HGS::Scene& demoScene)
+enum class Scenes
 {
-    demoScene.clear();
-    demoScene.add(HGS::LabelFactory({0.f, 0.f, 640.f, 480.f}, "Demo!", *defont, {0xff, 0xff, 0xff, 0x00},
-                               {0xff, 0x88, 0x00, 0xff}));
-    demoScene.add(HGS::BoxFactory({10.f, 20.f, 100.f, 100.f}, {0xff, 0x00, 0xff, 0xff}));
-    demoScene.add(HGS::BoxFactory({300.f, 100.f, 42.f, 300.f}, {0xff, 0xff, 0xff, 127}));
-    demoScene.add(HGS::CircleFactory({150.f, 20.f}, 49.f, {0xff, 0x00, 0x00, 0xff}));
+    None = 0,
+    MainMenuScene,
+    PlayScene
+} activeScene;
+
+struct MMSceneHandle
+{
+    HGS::Scene scene;
+    HGS::Widget* playBtn;
+    HGS::Widget* extBtn;
+} mainMenuHandle;
+
+void InitMainMenuHandle(struct MMSceneHandle& mmh)
+{
+    mmh.scene.clear();
+    mmh.scene.add(LabelFactory({100.f, 200.f, 150.f, 75.f}, "PLAY", *defont,
+                               {0x00, 0x00, 0xff, 0xff}, {0xff, 0x88, 0x00, 0xff}));
+    mmh.scene.add(LabelFactory({400.f, 200.f, 150.f, 75.f}, "EXIT", *defont,
+                               {0xff, 0x88, 0x00, 0xff}, {0x00, 0x00, 0xff, 0xff}));
+    mmh.playBtn = mmh.scene.get(0);
+    mmh.extBtn = mmh.scene.get(1);
+    mmh.extBtn->setCallBack([] { activeScene = Scenes::PlayScene; });
+}
+
+struct DemoSceneHandle
+{
+    HGS::Scene scene;
+    HGS::Widget* box;
+} demoSceneHandle;
+
+void InitDemoSceneHandle(struct DemoSceneHandle& dsh)
+{
+    dsh.scene.clear();
+    dsh.scene.add(HGS::LabelFactory({0.f, 0.f, 640.f, 480.f}, "Demo!", *defont,
+                                    {0xff, 0xff, 0xff, 0x00}, {0xff, 0x88, 0x00, 0xff}));
+    dsh.scene.add(HGS::BoxFactory({10.f, 20.f, 100.f, 100.f}, {0xff, 0x00, 0xff, 0xff}));
+    dsh.scene.add(HGS::BoxFactory({300.f, 100.f, 42.f, 300.f}, {0xff, 0xff, 0xff, 127}));
+    dsh.scene.add(HGS::CircleFactory({150.f, 20.f}, 49.f, {0xff, 0x00, 0x00, 0xff}));
+    dsh.box = dsh.scene.get(1);
+    dsh.box->setCallBack([&dsh] {
+        dsh.scene.add(HGS::BoxFactory({static_cast<float>(std::rand() % 600) + 20.f,
+                                       static_cast<float>(std::rand() % 440) + 20.f, 20.f, 20.f},
+                                      {static_cast<Uint8>(std::rand() % 0xff),
+                                       static_cast<Uint8>(std::rand() % 0xff),
+                                       static_cast<Uint8>(std::rand() % 0xff), 0xff}));
+    });
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
 {
-
     std::srand(std::time(0));
-    auto srand_lmbd{std::rand};
 
     if (const auto rc{HGS::ENG::init()}; rc != HGS::RC::OK) {
         std::cerr << "SDL_CANT_INIT\n";
@@ -56,82 +94,64 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     SDL_Event e{};
     float mx;
     float my;
-    bool mainMenuActive{true};
 
-    HGS::Scene mainMenu;
-    mainMenu.add(LabelFactory({100.f, 200.f, 150.f, 75.f}, "PLAY", *defont,
-                              {0x00, 0x00, 0xff, 0xff}, {0xff, 0x88, 0x00, 0xff}));
-    mainMenu.add(LabelFactory({400.f, 200.f, 150.f, 75.f}, "EXIT", *defont,
-                              {0xff, 0x88, 0x00, 0xff}, {0x00, 0x00, 0xff, 0xff}));
-    HGS::Widget* btn{mainMenu.get(0)};
-    HGS::Widget* ext{mainMenu.get(1)};
-
-    HGS::Scene demoScene;
-    fillDemoScene(demoScene);
-    HGS::Widget* box{demoScene.get(1)};
-    box->setCallBack([&demoScene, &srand_lmbd]() {
-        demoScene.add(HGS::BoxFactory({static_cast<float>(srand_lmbd() % 600) + 20.f,
-                                  static_cast<float>(srand_lmbd() % 440) + 20.f, 20.f, 20.f},
-                                 {static_cast<Uint8>(srand_lmbd() % 0xff),
-                                  static_cast<Uint8>(srand_lmbd() % 0xff),
-                                  static_cast<Uint8>(srand_lmbd() % 0xff), 0xff}));
-    });
+    activeScene = Scenes::MainMenuScene;
+    InitMainMenuHandle(mainMenuHandle);
 
     SDL_Log("\n\n==== MAIN LOOP ===\n\n");
-    while (main_loop) {
+    while (activeScene != Scenes::None) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
                 main_loop = false;
             } else if (e.type == SDL_EVENT_KEY_UP) {
                 auto k{e.key.key};
                 if (k == SDLK_Q || k == SDLK_ESCAPE) {
-                    if (mainMenuActive) {
-                        main_loop = false;
+                    if (activeScene == Scenes::MainMenuScene) {
+                        activeScene = Scenes::None;
                     } else {
-                        mainMenuActive = true;
+                        activeScene = Scenes::MainMenuScene;
                     }
                 }
             } else if (e.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
                 SDL_GetMouseState(&mx, &my);
-                if (mainMenuActive) {
-                    if (btn->isContains(mx, my)) {
-                        mainMenuActive = false;
-                        fillDemoScene(demoScene);
-                        box = demoScene.get(1);
-                        box->setCallBack([&demoScene, &srand_lmbd]() {
-                            demoScene.add(HGS::BoxFactory(
-                                {static_cast<float>(srand_lmbd() % 600) + 20.f,
-                                 static_cast<float>(srand_lmbd() % 440) + 20.f, 20.f, 20.f},
-                                {static_cast<Uint8>(srand_lmbd() % 0xff),
-                                 static_cast<Uint8>(srand_lmbd() % 0xff),
-                                 static_cast<Uint8>(srand_lmbd() % 0xff), 0xff}));
-                        });
-                    } else if (ext->isContains(mx, my)) {
-                        main_loop = false;
+                if (activeScene == Scenes::MainMenuScene) {
+                    if (mainMenuHandle.playBtn->isContains(mx, my)) {
+                        activeScene = Scenes::PlayScene;
+                        InitDemoSceneHandle(demoSceneHandle);
+                    } else if (mainMenuHandle.extBtn->isContains(mx, my)) {
+                        activeScene = Scenes::None;
                     }
                 } else {
-                    if (box->isContains(mx, my)) {
-                        dynamic_cast<HGS::Box*>(box)->setBG({0x00, 0x00, 0xff, 0xff});
+                    if (demoSceneHandle.box->isContains(mx, my)) {
+                        dynamic_cast<HGS::Box*>(demoSceneHandle.box)
+                            ->setBG({0x00, 0x00, 0xff, 0xff});
                     } else {
-                        box->setPos({mx, my});
-                        dynamic_cast<HGS::Box*>(box)->setBG({0xff, 0x00, 0x00, 0xff});
+                        demoSceneHandle.box->setPos({mx, my});
+                        dynamic_cast<HGS::Box*>(demoSceneHandle.box)
+                            ->setBG({0xff, 0x00, 0x00, 0xff});
                     }
-                    box->call2back();
+                    demoSceneHandle.box->call2back();
                 }
             }
             SDL_SetRenderDrawBlendMode(r, SDL_BLENDMODE_BLEND);
             SDL_SetRenderDrawColor(r, 0x00, 0x00, 0x00, 0xff);
             SDL_RenderClear(r);
-            if (mainMenuActive) {
-                mainMenu.render(r);
-            } else {
-                demoScene.render(r);
+            switch (activeScene) {
+            case Scenes::MainMenuScene:
+                mainMenuHandle.scene.render(r);
+                break;
+            case Scenes::PlayScene:
+                demoSceneHandle.scene.render(r);
+            default:
+                // todo
+                break;
             }
             SDL_RenderPresent(r);
         }
     }
-    demoScene.clear();
-    mainMenu.clear();
+
+    demoSceneHandle.scene.clear();
+    mainMenuHandle.scene.clear();
     HGS::ENG::deinit();
     return 0;
 }
